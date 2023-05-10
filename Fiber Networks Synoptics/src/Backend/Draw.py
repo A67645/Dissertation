fcf765ea177c4a240b1988dd47fd873f1375c131
A67645 <43671696+A67645@users.noreply.github.com>
2023-05-10 16:20:54 +0100

    Atributos visíveis introduzidos

--- Fiber Networks Synoptics/src/Backend/Draw.py
+++ Fiber Networks Synoptics/src/Backend/Draw.py
@@ -4,6 +4,7 @@ import ezdxf
 from ezdxf.addons.drawing import RenderContext, Frontend
 from ezdxf.addons.drawing.matplotlib import MatplotlibBackend
 from ezdxf.addons.drawing.properties import Properties, LayoutProperties
+from ezdxf.enums import TextEntityAlignment
 
 
 class Drawing:
@@ -27,10 +28,12 @@ class Drawing:
             block.add_circle((0, 0), 0.5, dxfattribs={'layer': "SINOP_PDO"})
         elif equipment == "JSO" or equipment == "JFO":
             block.add_circle((0, 0), 0.5, dxfattribs={'layer': "SINOP_JSFO"})
+        return block
 
     def blockref(self, name: str, coords, dxfattribs: dict, attribs: dict):
         blockref = self.modelspace.add_blockref(name, coords, dxfattribs)
         blockref.add_auto_attribs(attribs)
+        return blockref
 
     def draw(self, filename: str, path: str):
         layers = [
@@ -41,15 +44,26 @@ class Drawing:
         ]
         self.layers(layers)
         self.modelspace.add_text("Synoptics Map Prototype", height=2, dxfattribs={'layer': '0'}).set_placement((25, 20))
-        self.block("JSO", "JSO")
-        self.block("PDO", "PDO")
-        self.block("JFO", "JFO")
-        self.blockref("JSO", (0, 0), {'layer': 'SINOP_JSFO', 'xscale': 10, 'yscale': 10}, {'NAME': "JSO%d" % 200})
-        self.blockref("JFO", (60, -60), {'layer': 'SINOP_JSFO', 'xscale': 10, 'yscale': 10}, {'NAME': "JFO%d" % 200})
-        self.blockref("PDO", (60, 0), {'layer': 'SINOP_PDO', 'xscale': 10, 'yscale': 10}, {'NAME': "PDO%d" % 2001})
-        self.blockref("PDO", (120, -60), {'layer': 'SINOP_PDO', 'xscale': 10, 'yscale': 10}, {'NAME': "PDO%d" % 2002})
-        self.blockref("PDO", (120, -120), {'layer': 'SINOP_PDO', 'xscale': 10, 'yscale': 10}, {'NAME': "PDO%d" % 2003})
-        self.blockref("PDO", (60, -120), {'layer': 'SINOP_PDO', 'xscale': 10, 'yscale': 10}, {'NAME': "PDO%d" % 2004})
+        jso = self.block("JSO", "JSO")
+        pdo = self.block("PDO", "PDO")
+        jfo = self.block("JFO", "JFO")
+        jso200 = self.blockref("JSO", (0, 0), {'layer': 'SINOP_JSFO', 'xscale': 10, 'yscale': 10}, {'NAME': "JSO%d" % 200})
+        jfo200 = self.blockref("JFO", (60, -60), {'layer': 'SINOP_JSFO', 'xscale': 10, 'yscale': 10}, {'NAME': "JFO%d" % 200})
+        pdo2001 = self.blockref("PDO", (60, 0), {'layer': 'SINOP_PDO', 'xscale': 10, 'yscale': 10}, {'NAME': "PDO%d" % 2001})
+        pdo2002 = self.blockref("PDO", (120, -60), {'layer': 'SINOP_PDO', 'xscale': 10, 'yscale': 10}, {'NAME': "PDO%d" % 2002})
+        pdo2003 = self.blockref("PDO", (120, -120), {'layer': 'SINOP_PDO', 'xscale': 10, 'yscale': 10}, {'NAME': "PDO%d" % 2003})
+        pdo2004 = self.blockref("PDO", (60, -120), {'layer': 'SINOP_PDO', 'xscale': 10, 'yscale': 10}, {'NAME': "PDO%d" % 2004})
+        jso200.add_attrib('TIPO', 'JSO200').set_placement((0, 8), align=TextEntityAlignment.MIDDLE_CENTER)
+        jfo200.add_attrib('TIPO', 'JFO200').set_placement((60, -52), align=TextEntityAlignment.MIDDLE_CENTER)
+        jfo200.add_attrib('CABO', 'VDF-BRA1-JSO200-95001').set_placement((52, -60), align=TextEntityAlignment.BOTTOM_RIGHT)
+        pdo2001.add_attrib('TIPO', 'PDO2001').set_placement((60, 8), align=TextEntityAlignment.MIDDLE_CENTER)
+        pdo2001.add_attrib('CABO', 'VDF-BRA1-95001').set_placement((52, 0), align=TextEntityAlignment.BOTTOM_RIGHT)
+        pdo2002.add_attrib('TIPO', 'PDO2002').set_placement((120, -52), align=TextEntityAlignment.MIDDLE_CENTER)
+        pdo2002.add_attrib('CABO', 'VDF-BRA1-JSO200-95002').set_placement((112, -60), align=TextEntityAlignment.BOTTOM_RIGHT)
+        pdo2003.add_attrib('TIPO', 'PDO2003').set_placement((120, -112), align=TextEntityAlignment.MIDDLE_CENTER)
+        pdo2003.add_attrib('CABO', 'VDF-BRA1-JSO200-95003').set_placement((112, -120), align=TextEntityAlignment.BOTTOM_RIGHT)
+        pdo2004.add_attrib('TIPO', 'PDO2004').set_placement((60, -112), align=TextEntityAlignment.MIDDLE_CENTER)
+        pdo2004.add_attrib('CABO', 'VDF-BRA1-JSO200-95004').set_placement((52, -120), align=TextEntityAlignment.BOTTOM_RIGHT)
         self.line((0, 0), (60, 0), 'SINOP_CONDUTA')
         self.line((0, 0), (60, -60), 'SINOP_AEREO')
         self.line((0, 0), (60, -120), 'SINOP_AEREO')
